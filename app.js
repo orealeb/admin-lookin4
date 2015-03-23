9ce0f28eb914650e47162b118c40d25d58984bf0
unknown <alebios2@SIEBL-4303-06.ad.uillinois.edu>
2015-03-23 03:14:51 -0500

    lookin4 favicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -10,10 +10,12 @@ var mongoose = require('mongoose');
 mongoose.connect("mongodb://orealeb:potatoman@ds031627.mongolab.com:31627/heroku_app34833306");
 
 var app = express();
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 var debug = require('debug')('passport-mongo');
 //var app = require('../app');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
@@ -22,7 +24,6 @@ var server = app.listen(app.get('port'), function() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
